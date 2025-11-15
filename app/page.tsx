faa74ca0876658a76cc254f81bf8eb0550a2250f
lucasbaruj4 <barujalucas0@gmail.com>
2025-11-15 16:05:50 +0100

    more file methods added, page.tsx now handles commands when enter is pressed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
-import Image from "next/image";
+'use client';
+import CreateFile from "@src/file";
+import { appendContent } from "@src/file";
+import { useState } from "react";
 
-export default function Terminal() {
+export default function terminal() {
+  const [userInput, setUserInput] = useState("");
+
+  const detectKey = (e: any) => {
+    if (e.key == "Enter") {
+      userInputSent(userInput);
+    }
+  }
+
+  const userInputSent = (userInput: string) => {
+    console.log("el comando es: ", userInput);
+  }
+
 
   return (
-    <div id="prompt" className="container grid grid-cols-[auto_1fr] gap-1 text-white p-3 text-xl py-5">
-      <div id="pwd" className="col-start-1 text-left text-3xl font-[Terminal]">
-        ~/some/direction {">"}&nbsp; 
+    <div onKeyDown={detectKey}>
+      <div id="prompt" className="container grid grid-cols-[auto_1fr] gap-1 text-white p-3 text-xl py-5">
+        <div id="pwd" className="col-start-1 text-left text-3xl font-[Terminal]">
+          ~/some/direction {">"}&nbsp;
+        </div>
+        <input id="user_prompt" placeholder="cat README" autoFocus type="text" className="col-start-2 text-left text-3xl font-[Terminal] outline-none  caret_transparent" onChange={(e) => setUserInput(e.target.value)} />
       </div>
-      <input id="user_prompt" placeholder="cat README" autoFocus type="text" className="col-start-2 text-left text-3xl font-[Terminal] outline-none  caret_transparent"  />
     </div>
   )
 }
--- src/file.tsx
+++ src/file.tsx
@@ -1,9 +1,29 @@
-export default function CreateFile(name : string, content : string) {
+class File {
+  name: string;
+  content?: string;
 
+  constructor(name: string, content?: string) {
+    this.name = name;
+    this.content = content;
+  }
 
+  appendContent(content: string) {
+    this.content = content;
+  }
 }
 
-export function File() {
 
+export default function CreateFile(name: string) {
+  var newFile: File = new File(name);
+  console.log("The file has been created");
+  return newFile;
 }
+
+export function appendContent(file: File, content: string) {
+  file.appendContent(content);
+  console.log("The content has been appended to the file");
+  return file;
+}
+
+
 
